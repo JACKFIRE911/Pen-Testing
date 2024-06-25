63053413fcbc571fe750bfd698ed209440967b0b
JACK FIRE <109713399+JACKFIRE911@users.noreply.github.com>
2024-06-25 08:18:03 +0530

    Add files via upload

---
+++ req.js
@@ -0,0 +1,8 @@
+<script>
+
+function httpGet(theUrl){
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
